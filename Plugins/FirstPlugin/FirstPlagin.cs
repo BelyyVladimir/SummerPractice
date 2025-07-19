29d8b78d2acca35c1f21340cf3ff9c2d2d969d5f
BelyyVladimir <vladimrbelyy@yandex.ru>
2025-07-19 23:39:15 +0600

    feet: add task10

---
+++ Plugins/FirstPlugin/FirstPlagin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PluginInterface;
+using PluginAttributes;
+
+namespace FirstPlugin
+{
+    [PluginLoadAttribute]
+    public class FirstPlugin : IPlugin
+    {
+        public void Execute()
+        {
+            Console.WriteLine("First plugin is executing");
+        }
+    }
+}
